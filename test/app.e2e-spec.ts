0f014a474b8fdfe9f272793706ff427cefb530d4
Fabien Ruffin <fabien.ruffin@outlook.com>
2022-08-02 22:30:19 +1000

    Added tests for Kanban API exercise

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -22,3 +22,53 @@ describe('AppController (e2e)', () => {
       .expect('Hello World!');
   });
 });
+
+describe('BoardController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/cards (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/cards')
+      .send({
+        title: 'Create a new project',
+      })
+      .expect(201)
+      .expect({
+        id: 1,
+        status: 1,
+        title: 'Create a new project',
+      });
+  });
+
+  it('/cards/:id (PUT)', () => {
+    return request(app.getHttpServer())
+      .put('/cards/1')
+      .send({
+        status: 2,
+      })
+      .expect(200)
+      .expect({
+        id: 1,
+        status: 2,
+        title: 'Create a new project',
+      });
+  });
+
+  it('/cards/:id (PUT) invalid status', () => {
+    return request(app.getHttpServer())
+      .put('/cards/1')
+      .send({
+        status: 4,
+      })
+      .expect(400);
+  });
+});
